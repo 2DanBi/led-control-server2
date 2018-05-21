d5881605ee7733cba551e1da3670de91b301622e
danJJang <danbi9612@gmail.com>
2018-05-21 10:35:25 +0900

    LED 조작 추가

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const ejs = require('ejs')
 const clc = require('cli-color')
+const onoff = require('onoff')
 
 const app = express()
+const gpio = onoff.Gpio
+
+const led1 = new gpio(6, 'out')
+const led2 = new gpio(13, 'out')
 
 app.set('views', __dirname)
 app.set('view engine', 'ejs')
@@ -12,6 +17,18 @@ app.use(express.static('public'))
 
 app.get('/', (req, res, next) => {
     res.render('index.html', { msg:"hello" })
+})
+
+app.get('/ledon', (req, res, next) => {
+    led1.writeSync(1)
+    led2.writeSync(1)
+    res.send('led on - ok')
+})
+
+app.get('/ledoff', (req, res, next) => {
+    led1.writeSync(0)
+    led2.writeSync(0)
+    res.send('led off - ok')
 })
 
 app.listen(8080, function() {
