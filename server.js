309f841a8acd12210c1aa43021d6e7629da7908d
danJJang <danbi9612@gmail.com>
2018-05-21 10:07:19 +0900

    웹서버기본세팅

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
+const express = require('express')
+const ejs = require('ejs')
 const clc = require('cli-color')
-console.log(clc.red('hello'))
+
+const app = express()
+
+app.set('views', __dirname)
+app.set('view engine', 'ejs')
+app.engine('html', ejs.renderFile)
+
+app.use(express.static('public'))
+
+app.get('/', (req, res, next) => {
+    res.render('index.html', { msg:"hello" })
+})
+
+app.listen(8080, function() {
+    console.log(clc.green('Ready webserver'))
+})
+
